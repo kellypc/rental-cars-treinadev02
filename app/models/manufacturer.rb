a845948468054907169bcbb73ca8d913e7193763
kellypc <kellycruz.ti@gmail.com>
2019-12-04 20:59:42 -0300

    admin-register-car-model

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,4 +1,6 @@
 class Manufacturer < ApplicationRecord
   validates :name, presence: { message: 'Não pode ficar em branco'}
   validates :name, uniqueness: { message: 'Nome já está em uso'}
+
+  has_many :car_models
 end
